b7cad92b821dcfbd19f4764e38132f1cf0f97bdd
Eugene Kardash <kardash675@gmail.com>
2024-12-16 12:13:09 +0300

    Initial Commit

---
+++ Taks/TaksApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaksApp.swift
+//  Taks
+//
+//  Created by Taqtile on 16.12.24.
+//
+
+import SwiftUI
+
+@main
+struct TaksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
